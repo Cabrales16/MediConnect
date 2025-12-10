2e4fc9e37882013c8113760104da6f63d5d46a32
Cabrales16 <andrescabrales322@gmail.com>
2025-12-10 18:19:22 -0500

    adjunto cambios para railway

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,13 +1,20 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:admin@localhost:3315/agendamiento_de_citas'
+# EN PRODUCCIÓN (Railway) leerá DATABASE_URL.
+# EN LOCAL, si no existe esa variable, usará tu URL de siempre.
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "mysql+pymysql://root:admin@localhost:3315/agendamiento_de_citas"
+)
 
-
-
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True,   # evita conexiones muertas
+    pool_recycle=3600,
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
