1772d218b85731e49ef6bc3d307e670ab65a64b2
Cabrales16 <andrescabrales322@gmail.com>
2025-12-10 18:58:32 -0500

    cambios para desplegar frontend sobre viteconfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,12 @@
+// frontend/vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: "0.0.0.0",
+    port: 4173,
+    allowedHosts: ["athletic-courtesy-production.up.railway.app"]
+  },
 });
